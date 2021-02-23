61df3446ceaffaccab63f8e25d907408cf92b29a
BuildTools <olmorris@ucalgary.ca>
2021-02-22 17:40:58 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private int i;
+
+    // Start is called before the first frame update
+    void Start() {
+        i = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        i++;
+        Debug.Log("Frames: " + i);
+    }
+}
